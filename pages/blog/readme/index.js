e1dec0155623310e3e7c893fbf9fc51bf14f2d7f
zianonlyhk <58275873+zianonlyhk@users.noreply.github.com>
2021-09-09 15:51:19 +0100

    correcting typos in blog template

--- pages/blog/readme/index.js
+++ pages/blog/readme/index.js
@@ -32,22 +32,23 @@ export default function Home() {
           <p>
             This is my personal website for me to put my works and drawings
             online for people to access. I also made a blog session to motivate
-            me to write some study blogs on maths, physics and programming. I do
-            feel like I can understand the topics way better and get new
-            insights when I am trying to reproduce new concepts I learnt. Wish I
-            am not too lazy or busy so I can keep friends and peers updated.
+            myself to write some study blogs on maths, physics and programming.
+            I do feel like I can understand the topics way better and get new
+            insights when I am trying to reproduce new concepts I learnt. Wish
+            me not being too lazy or busy so I can keep friends and peers
+            updated.
           </p>
           <p>
-            This website was run on Next.js framework. The source code was put
-            on GitHub then fetched and deployed by Vercel, which is the
-            recommended method for Next.js project.
+            This website is running on Next.js framework. The source code was
+            put on GitHub then fetched and deployed by Vercel, which is the
+            recommended deployment method for Next.js project.
           </p>
           <p>
             The name of the page - Room 214N, was inspired by the room number I
             stayed in Connaught Hall, a student accommodation in London, in my
             first 2 years of undergrad. In fact I would be there in my third
-            year as well. There are a few other details in the choice so try to
-            see if you can figure out.
+            year as well. There are a few other references in the choice so try
+            to see if you can figure out.
           </p>
           <p>
             I have been trash talking so far cuz this blog page is actually a
@@ -59,8 +60,7 @@ export default function Home() {
           </p>
           <p>
             Humm if I really have the feeling, I will add an interface in
-            Chinese as well. Who knows - if I will start my career back home
-            then.
+            Chinese as well. Who knows - if I will start my career back home.
           </p>
           {/* add contents between */}
         </div>
